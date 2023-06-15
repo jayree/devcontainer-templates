eb84a8f8d708d68626f60e2b9dfe8ace3c8d979b
jayree <jayree.git@icloud.com>
2023-06-15 02:02:14 +0200

    Initial commit

---
+++ test/color/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+cd $(dirname "$0")
+source test-utils.sh
+
+# Template specific tests
+check "distro" lsb_release -c
+check "color" [ $(cat /tmp/color.txt | grep red) ]
+
+# Report result
+reportResults
---
+++ test/hello/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+cd $(dirname "$0")
+source test-utils.sh
+
+# Template specific tests
+check "distro" lsb_release -c
+check "greeting" [ $(cat /usr/local/etc/greeting.txt | grep hey) ]
+
+# Report result
+reportResults
