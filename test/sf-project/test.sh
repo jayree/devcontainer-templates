2684f2f977434fdb99f68e78ee0c418a9809e9f7
jayree <jayree.git@icloud.com>
2023-06-23 15:36:00 +0200

    fix: add SFDX_CONTAINER_MODE

--- test/sf-project/test.sh
+++ test/sf-project/test.sh
@@ -7,7 +7,8 @@ source test-utils.sh
 
 # Template specific tests
 check "distro" lsb_release -c
-check "validate zshrc" cat /home/vscode/.zshrc | grep "SF_AC_ZSH_SETUP_PATH"
+check "zshrc" bash -c "cat /home/vscode/.zshrc | grep SF_AC_ZSH_SETUP_PATH"
+check "container mode" bash -c "echo $SFDX_CONTAINER_MODE | grep true"
 
 # Report result
 reportResults
